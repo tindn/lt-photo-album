fe8851633939e9280befc304a147145947962c88
Tin Nguyen <tnguyen@qci.com>
2017-03-04 14:56:33 -0600

    moved photo class to a separate file

---
+++ dotnet/photo-album/photo-album/Photo.cs
@@ -0,0 +1,16 @@
+﻿namespace photo_album
+{
+    public class Photo
+    {
+        public int AlbumId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{this.Id}] {this.Title}";
+        }
+    }
+}
--- dotnet/photo-album/photo-album/Program.cs
+++ dotnet/photo-album/photo-album/Program.cs
@@ -101,18 +101,4 @@ Default starting position is 1. For example, '-s 5' to start from the fifth phot
             return displayCount > photosCount - startIndex ? photosCount : displayCount + startIndex - 1;
         }
     }
-
-    class Photo
-    {
-        public int AlbumId { get; set; }
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Url { get; set; }
-        public string ThumbnailUrl { get; set; }
-
-        public override string ToString()
-        {
-            return $"[{this.Id}] {this.Title}";
-        }
-    }
 }
