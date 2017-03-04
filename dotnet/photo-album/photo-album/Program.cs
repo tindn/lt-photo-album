b7a30343d632f0360a2178071e8610d9ffcf0fc8
Tin Nguyen <tnguyen@qci.com>
2017-03-04 09:09:12 -0600

    added regex to allow for additional arguments for display count and starting position

--- dotnet/photo-album/photo-album/Program.cs
+++ dotnet/photo-album/photo-album/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace photo_album
 {
@@ -14,22 +15,38 @@ namespace photo_album
             if (args.Length == 0 || args[0] == "-h")
             {
                 Console.WriteLine("Use photo-album [id] to get a list of photos for certain photo album");
-                Console.Write("For example 'photo-album 3' to get photos of album 3");
+                Console.WriteLine("For example 'photo-album 3' to get photos of album 3");
+                Console.WriteLine(@"You can use argument -n to specify the number of photos to display. 
+                                    Default number is 10.");
+                Console.WriteLine(@"You can use argument -s to specify the starting position of the photos. 
+                                    Default starting position is 1.");
+                return;
+            }
+            var inputRegex = new Regex(@"(\d+){1}(\s{1}-n\s(\d+))?(\s{1}-s\s(\d+))?");
+            var inputString = string.Join(" ", args);
+            var match = inputRegex.Match(inputString);
+            if (!match.Success)
+            {
+                Console.WriteLine("Incorrect format");
                 return;
             }
 
-            int albumId;
-            if (!int.TryParse(args[0], out albumId))
+            int albumId = int.Parse(match.Groups[1].Value);
+            Console.WriteLine($"Album Id: {albumId}");
+            var displayCount = match.Groups[3].Success ? int.Parse(match.Groups[3].Value) : 10;
+            var startIndex = match.Groups[5].Success ? int.Parse(match.Groups[5].Value) : 0;
+            var responseString = GetAlbumJsonString(albumId);
+            var photos = JsonConvert.DeserializeObject<List<Photo>>(responseString);
+            Console.WriteLine($"Album {albumId} has {photos.Count} photos");
+            if (startIndex > photos.Count)
             {
-                Console.WriteLine("Invalid album id.");
+                Console.WriteLine("The starting position exceeds the number of photos in album");
                 return;
             }
-            Console.WriteLine($"Album Id: {albumId}");
-            var responseString = GetAlbumJsonString(albumId);
-            var albums = JsonConvert.DeserializeObject<List<Album>>(responseString);
-            Console.WriteLine($"Album {albumId} has {albums.Count} photos");
-            Console.WriteLine($"Showing the first 10 photos");
-            albums.Take(10).ToList().ForEach(a =>
+            var displayEnd = displayCount > photos.Count - startIndex ? photos.Count 
+                : displayCount + startIndex - 1;
+            Console.WriteLine($"Showing photos from {startIndex} to {displayEnd}");
+            photos.Skip(startIndex -1).Take(displayCount).ToList().ForEach(a =>
             {
                 Console.WriteLine(a.Display());
             });
@@ -47,7 +64,7 @@ namespace photo_album
         }
     }
 
-    class Album
+    class Photo
     {
         public int AlbumId { get; set; }
         public int Id { get; set; }
